b409acd14c906c2031c1424f864633f1b99e25ba
Philip <philipmjobe@gmail.com>
2021-10-20 15:28:22 -0400

    added users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,7 @@
 class UsersController < ApplicationController
   def create
-    user = User.new{
-      first_name = params[:first_name]
-      last_name = params[:last_name]
-      email = params[:email]
-    }
-    user.save
+    user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email])
+    user.save 
     render json: user
   end 
 
