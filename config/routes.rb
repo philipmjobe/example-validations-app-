b44235dd56745fb1e41d9bc681caf20d276520c1
Philip <philipmjobe@gmail.com>
2021-10-21 09:07:07 -0400

    updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post "/users" => "users#create" 
+  get "/users" => "users#index"
+  get "/users" => "users#show"
+  post "/users" => "users#create"
+  patch "/users/:id" => "users#update"
+  delete "/users/:id" => "users#destroy" 
 end
