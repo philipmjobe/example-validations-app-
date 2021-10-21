20b2a804ecc84366e1b2785dd5412e5d71800173
Philip <philipmjobe@gmail.com>
2021-10-21 09:19:07 -0400

    updated users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,41 @@
 class UsersController < ApplicationController
+
+  def index 
+    users = User.all 
+    render json: users.as_json
+  end 
+
+  def show
+    users = User.find_by[:id]
+    render json: users.as_json
+  end 
+  
   def create
     user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email])
-    user.save 
-    render json: user
+    
+    if user.save 
+      render json: user.as_json
+    else  
+      render json: {errors: user.errors.full_messages }, status:
+    end 
+  end 
+
+  def update
+    user = User.find_by["id"]
+    user.first_name = params["first_name"] || user.first_name
+    user.last_name = params["last_name"] || user.last_name
+    user.email = params["email"] || user.email
+
+    if user.save
+      render json: user.as_json
+    else
+      render json: {errors: user.errors.full_messages}, status:
+    end 
+  end 
+
+  def destroy
+    User.destroy_by(id: params["id"])
+    render json: {message: "User Destroyed"}
   end 
 
 end
